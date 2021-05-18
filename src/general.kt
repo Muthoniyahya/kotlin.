d53e9c4938a1f9d411a083e72808e0ba39d6a714
student <Muthoniyahya@gmail.com>
2021-05-18 17:57:16 +0300

    first commit

---
+++ src/general.kt
@@ -0,0 +1,39 @@
+
+fun main() {
+
+    Book("Half of a yellow sun","Chimamanda Adichie",280)
+    Book("Purple Hibiscus","Ngozi Adichie",200)
+    Book("Born a Crime","Trevor Noah",280)
+    Track("KCA",120,14)
+
+
+
+
+
+
+
+}
+data class Book(var Title:String, var Author:String,var pages:Int){
+
+
+
+
+//    fun book(book1:String,book2: String,book3: String){
+//
+
+
+
+
+
+}
+class Track(registration:String, totalWeight:Int,capacity:Int){
+    fun load(weight:Int) {
+
+
+
+
+
+
+
+    }
+}
